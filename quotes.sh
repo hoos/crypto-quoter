fc7e0d472322ea8e4a0b2146186c17e34e1017ae
Hussein Badakhchani <hussein.badakhchani@esqimo.com>
2019-10-14 19:20:49 +0100

    Added fees based on listed  market taker rates

--- quotes.sh
+++ quotes.sh
@@ -40,7 +40,6 @@ btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
 totalusd=$(addfee "1.0025" "$priceusd")
 print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp "0.25%" $totalusd 
 
-
 exchange="COINBASEPRO" 
 symbolusd="BTC-USD"
 curl -s https://api.pro.coinbase.com/products/BTC-USD/book  > ./$DATA_DIR/${exchange}_${symbolusd}.json
@@ -51,7 +50,8 @@ curl -s https://api.pro.coinbase.com/products/BTC-GBP/book  > ./$DATA_DIR/${exch
 pricegbp=`jq .asks[0][0]  ./$DATA_DIR/${exchange}_${symbolgbp}.json`
 pricegbp=$(strip_quotes "$pricegbp")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0025" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp "0.25%" $totalusd 
 
 #KRAKEN
 exchange="KRAKEN" 
@@ -65,7 +65,8 @@ curl -s https://api.kraken.com/0/public/Ticker?pair=$symbolgbp > ./$DATA_DIR/${e
 pricegbp=`jq .result.XXBTZGBP.a[0] ./$DATA_DIR/${exchange}_${symbolgbp}.json`
 pricegbp=$(strip_quotes "$pricegbp")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0026" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp "0.26%" $totalusd 
 
 #BITFINEX
 exchange="BITFINEX" 
@@ -79,7 +80,8 @@ curl -s --request GET --url https://api.bitfinex.com/v1/pubticker/btcgbp > ./$DA
 pricegbp=`jq .last_price ./$DATA_DIR/${exchange}_${symbolgbp}.json`
 pricegbp=$(strip_quotes "$pricegbp")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0020" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp "0.20%" $totalusd 
 
 #WIREX
 exchange="WIREX" 
@@ -93,7 +95,8 @@ curl -s https://api.wirexapp.com/public/ticker > ./$DATA_DIR/${exchange}_${symbo
 pricegbp=`jq '.rates[] | select(.ticker == "BTC/GBP") | .ask' ./$DATA_DIR/${exchange}_${symbolgbp}.json`
 pricegbp=$(strip_quotes "$pricegbp")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.01" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp $pricegbp $symbolbtcusdbgp $btcusdgbp "1.0%" $totalusd
 
 # Binance Jersey
 exchange="BINANCE-JERSEY" 
@@ -102,7 +105,8 @@ symbolgbp="BTCGBP"
 curl -s https://api.binance.je/api/v3/avgPrice?symbol=$symbolgbp > ./$DATA_DIR/${exchange}_${symbolgbp}.json
 pricegbp=`jq .price $DATA_DIR/${exchange}_${symbolgbp}.json`
 pricegbp=$(strip_quotes "$pricegbp")
-print_quote $exchange $symbolusd "N/A" $symbolgbp $pricegbp $symbolbtcusdbgp "N/A"
+totalgbp=$(addfee "1.0010" "$pricegbp")
+print_quote $exchange $symbolusd "N/A" $symbolgbp $pricegbp $symbolbtcusdbgp "N/A" "0.1%" $totalgbp
 
 # Binance USD
 exchange="BINANCE" 
@@ -112,7 +116,8 @@ curl -s https://api.binance.com/api/v3/ticker/price?symbol=$symbolusd > ./$DATA_
 priceusd=`jq .price $DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0010" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.1%" $totalusd
 
 #Bitstamp
 exchange="BITSTAMP" 
@@ -126,7 +131,8 @@ priceusd=$(strip_quotes "$priceusd")
 #priceeur=$(strip_quotes "$priceeur")
 symbolgbp="BTCGBP"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0050" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.5%" $totalusd
 
 #Huobi-Global
 exchange="HUOBI-GLOBAL" 
@@ -135,7 +141,8 @@ curl -s https://api.huobi.pro/market/detail/merged?symbol=btcusdt > ./$DATA_DIR/
 priceusd=`jq .tick.ask[0] ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0070" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.7%" $totalusd
 
 #VINDAX
 exchange="VINDAX" 
@@ -145,7 +152,8 @@ priceusd=`jq '.[].last' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTCGBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0005" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.05%" $totalusd
 
 #P2PB2B
 exchange="P2PB2B" 
@@ -155,7 +163,8 @@ priceusd=`jq '.result.ask' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTC_GBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0020" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.2%" $totalusd
 
 #MXC
 exchange="MXC" 
@@ -165,7 +174,8 @@ priceusd=`jq '.data.last' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTC_GBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0010" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.1%" $totalusd
 
 #COINEGG
 exchange="COINEGG" 
@@ -175,7 +185,8 @@ priceusd=`jq '.last' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTC_GBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0010" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.1%" $totalusd
 
 #COINEAL
 exchange="COINEAL" 
@@ -185,7 +196,8 @@ priceusd=`jq '.data.last ' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTCGBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0015" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.15%" $totalusd
 
 #IDAX
 exchange="IDAX" 
@@ -195,19 +207,21 @@ priceusd=`jq '.ticker[].last' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTC_GBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0015" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.15%" $totalusd
 
 #FATBTC
-exchange="FATBTC" 
-symbolusd="BTCUSDT"
+#exchange="FATBTC" 
+#symbolusd="BTCUSDT"
 #curl -s https://www.fatbtc.com/m/trade/BTCUSDT/1/`date +%s` > ./$DATA_DIR/${exchange}_${symbolusd}.json
-TIMESTAMP=`date +%s`
-curl -s "https://www.fatbtc.com/m/trade/BTCUSDT/1/$TIMESTAMP" -H 'authority: www.fatbtc.com' -H 'cache-control: max-age=0' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'sec-fetch-site: none' -H 'accept-language: en-US,en;q=0.9' -H 'cookie: __cfduid=dffa23eb5799dfcf685b8caa274c8a6021570320591; _ga=GA1.2.834151933.1570320595; Hm_lvt_79ceeb6955b44b09df6a9c3fbb72555a=1570320596; cf_clearance=91ca8d2a5eb61e169e663680aecf56a2669f1275-1571071164-0-150' > ./$DATA_DIR/${exchange}_${symbolusd}.json
-priceusd=`jq '.trades[].price' ./$DATA_DIR/${exchange}_${symbolusd}.json`
-priceusd=$(strip_quotes "$priceusd")
-symbolgbp="BTC_GBPT"
-btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+#TIMESTAMP=`date +%s`
+#curl -s "https://www.fatbtc.com/m/trade/BTCUSDT/1/$TIMESTAMP" -H 'authority: www.fatbtc.com' -H 'cache-control: max-age=0' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'sec-fetch-site: none' -H 'accept-language: en-US,en;q=0.9' -H 'cookie: __cfduid=dffa23eb5799dfcf685b8caa274c8a6021570320591; _ga=GA1.2.834151933.1570320595; Hm_lvt_79ceeb6955b44b09df6a9c3fbb72555a=1570320596; cf_clearance=91ca8d2a5eb61e169e663680aecf56a2669f1275-1571071164-0-150' > ./$DATA_DIR/${exchange}_${symbolusd}.json
+#priceusd=`jq '.trades[].price' ./$DATA_DIR/${exchange}_${symbolusd}.json`
+#priceusd=$(strip_quotes "$priceusd")
+#symbolgbp="BTC_GBPT"
+#btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
+#totalusd=$(addfee "1.0020" "$priceusd")
+#print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.2%" $totalusd
 
 #BILAXY
 exchange="BILAXY" 
@@ -217,7 +231,8 @@ priceusd=`jq '.data.sell' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTCGBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.0015" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.15%" $totalusd
 
 #BITFOREX
 exchange="BITFOREX" 
@@ -227,4 +242,6 @@ priceusd=`jq '.data.sell' ./$DATA_DIR/${exchange}_${symbolusd}.json`
 priceusd=$(strip_quotes "$priceusd")
 symbolgbp="BTCGBPT"
 btcusdgbp=$(usdgbp "$priceusd" "$gbpusd")
-print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp
+totalusd=$(addfee "1.001" "$priceusd")
+print_quote $exchange $symbolusd $priceusd $symbolgbp "N/A" $symbolbtcusdbgp $btcusdgbp "0.1%" $totalusd
+
